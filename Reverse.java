9aeb65c6d639f3ab57d9c478a44e6cc99c158c38
Ohad Cohen <ohad.cohen02@post.runi.ac.il>
2023-12-26 20:18:51 +0200

    Ohad's Homework 2

--- Reverse.java
+++ Reverse.java
@@ -8,7 +8,7 @@ public class Reverse {
 		for(int i = (rev.length() - 1); i >= 0; i--){
 			System.out.print(rev.charAt(i));
 		}
-		if(rev.length%2 == 0){
+		if(rev.length()%2 == 0){
 			System.out.println("The middle character is " + rev.charAt((rev.length()/2) + 1));
 		}
 		else{
