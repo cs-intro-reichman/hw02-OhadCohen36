5b1a1d4ff3d4d88eed46d3844423a4ceb349f82c
Ohad Cohen <ohad.cohen02@post.runi.ac.il>
2023-12-26 20:40:00 +0200

    Ohad's Homework 2

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,20 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int num = Integer.parseInt(args[0]);
+		int sum = 0;
+		String perfect = num + " is a perfect number since " + num + " = 1";
+		for(int i = 1; i < num; i++ )
+		{
+			if(num%i == 0)
+			{
+				sum = sum + i;
+				perfect = perfect + " + " + i;
+			}
+		}
+		if {sum == num)
+			System.out.println(perfect);
+		else
+			System.out.println(num + " is not a perfect number");
 	}
 }
